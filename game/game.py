f391b521479558123ea14bf0fb49d1ad40e88038
= <=>
2022-05-16 16:35:04 +0300

    first-stable-server-bot-game-communication

--- game/game.py
+++ game/game.py
@@ -1,9 +1,6 @@
-from math import floor
-import os
 import socket
 import threading
-from time import sleep
-from tkinter import E
+from math import floor
 from typing import List, Set
 
 import numpy as np
@@ -12,12 +9,11 @@ from playsound import playsound
 
 pygame.init()
 
-PATH = os.getcwd() + "/files/"
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-BG_COLOR = (192, 192, 192)
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
 
+PATH = __file__[:-7] + "files/"
 IMAGE_NAMES = ["empty", "cell1", "cell2", "cell3", "cell4",
                "cell5", "cell6", "cell7", "cell8", "clicked_mine", "smile", "shocked", "cool", "dead", "settings", "mine"]
 
@@ -29,16 +25,17 @@ CELL_SPR = pygame.image.load(PATH + "cell.png")
 CLICKEDMINE_SPR = pygame.image.load(PATH + "clicked_mine.png")
 NOMINE_SPR = pygame.image.load(PATH + "nomine.png")
 
+BG_COLOR = (192, 192, 192)
+
 
 class Cell():
 
-    def __init__(self, gameId: int, value: int = 0, hidden: bool = True) -> None:
+    def __init__(self, value: int = 0, hidden: bool = True) -> None:
         self.__hidden: bool = hidden
         self.__flagged: bool = False
         self.__content: int = value
         self.x_pos: int = None
         self.y_pos: int = None
-        self.gameId = gameId
         self.debug = False  # Used for debugging
 
     @property
@@ -49,6 +46,12 @@ class Cell():
     def hidden(self, __value):
         if self.flagged:
             raise Exception("Can't dehide a flagged cell")
+        elif self.content == -1:
+            # threading.Thread(target=playsound, args=(PATH + "game-over.mp3",)).start()
+            for list in GAME.grid.contents:
+                for cell in list:
+                    if not cell.flagged:
+                        cell.__hidden = False
         else:
             self.__hidden = __value
 
@@ -78,53 +81,45 @@ class Cell():
             x (int): X coordinate to draw to.
             y (int): Y coordinate to draw to.
         """
-        self.rect = pygame.Rect(GAMES[self.gameId].settings["lrb_border_size"] + x * GAMES[self.gameId].settings["cell_size"],
-                                GAMES[self.gameId].settings["top_border_size"] + y * GAMES[self.gameId].settings["cell_size"], GAMES[self.gameId].settings["cell_size"], GAMES[self.gameId].settings["cell_size"])
+        self.rect = pygame.Rect(GAME.settings["lrb_border_size"] + x * GAME.settings["cell_size"],
+                                GAME.settings["top_border_size"] + y * GAME.settings["cell_size"], GAME.settings["cell_size"], GAME.settings["cell_size"])
 
         if self.debug:
-            GAMES[self.gameId].gameDisplay.blit(FLAG_SPR, self.rect)
+            GAME.gameDisplay.blit(FLAG_SPR, self.rect)
         elif self.hidden:
             if self.flagged:
-                GAMES[self.gameId].gameDisplay.blit(FLAG_SPR, self.rect)
+                GAME.gameDisplay.blit(FLAG_SPR, self.rect)
             else:
-                GAMES[self.gameId].gameDisplay.blit(CELL_SPR, self.rect)
+                GAME.gameDisplay.blit(CELL_SPR, self.rect)
         else:
-            GAMES[self.gameId].gameDisplay.blit(
+            GAME.gameDisplay.blit(
                 SPR_LIST[self.content], self.rect)
 
     def revealCell(self):
         """This method Reveals a cell and checks
         """
         try:
-            self.hidden = False
+            self.hidden = False    
             if self.content == -1:
                 self.content = 9
-                # threading.Thread(target=playsound, args=(PATH + "game-over.mp3",)).start()
-                for list in GAMES[self.gameId].grid.contents:
-                    for cell in list:
-                        if not cell.flagged:
-                            cell.hidden = False
-
+                GAME.playing = False
                 return
 
             cells_to_check: Set[Cell] = {self}
-            checked_cells: Set[Cell] = set()
             while len(cells_to_check) > 0:
                 cell = cells_to_check.pop()
                 if cell.saturated():
                     for y in range(-1, 2):
                         for x in range(-1, 2):
-                            if 0 <= cell.x_pos + x < GAMES[self.gameId].settings["width"] and 0 <= cell.y_pos + y < GAMES[self.gameId].settings["height"]:
-                                adj_cell = GAMES[self.gameId].grid.contents[cell.y_pos +
+                            if 0 <= cell.x_pos + x < GAME.settings["width"] and 0 <= cell.y_pos + y < GAME.settings["height"]:
+                                adj_cell = GAME.grid.contents[cell.y_pos +
                                                                             y][cell.x_pos + x]
                                 if not adj_cell.flagged:
                                     adj_cell.hidden = False
-                                    if adj_cell not in checked_cells:
+                                    if not adj_cell.checked():
                                         if cell.content == 0 or adj_cell.content == 0:
                                             cells_to_check.add(adj_cell)
 
-
-                checked_cells.add(cell)
         except Exception as e:
             print(e)
 
@@ -135,32 +130,43 @@ class Cell():
             bool: True if as much flags or more, False if less or the cell is a mine.
         """
         adj_flags = 0
-        if GAMES[self.gameId].grid.contents[self.y_pos][self.x_pos].content not in {0, 1, 2, 3, 4, 5, 6, 7, 8}:
+        if GAME.grid.contents[self.y_pos][self.x_pos].content not in {0, 1, 2, 3, 4, 5, 6, 7, 8}:
             return False
         else:
             for a in range(-1, 2):
                 for b in range(-1, 2):
-                    if 0 <= self.x_pos + a < GAMES[self.gameId].settings["width"] and 0 <= self.y_pos + b < GAMES[self.gameId].settings["height"]:
-                        if GAMES[self.gameId].grid.contents[self.y_pos + b][self.x_pos + a].flagged:
+                    if 0 <= self.x_pos + a < GAME.settings["width"] and 0 <= self.y_pos + b < GAME.settings["height"]:
+                        if GAME.grid.contents[self.y_pos + b][self.x_pos + a].flagged:
                             adj_flags += 1
 
-            if GAMES[self.gameId].grid.contents[self.y_pos][self.x_pos].content <= adj_flags:
+            if GAME.grid.contents[self.y_pos][self.x_pos].content <= adj_flags:
                 return True
             else:
                 return False
 
 
+    def checked(self) -> bool:
+        for y in range(-1, 2):
+            for x in range(-1, 2):
+                if 0 <= self.x_pos + x < GAME.settings["width"] and 0 <= self.y_pos + y < GAME.settings["height"]:
+                    adj_cell = GAME.grid.contents[self.y_pos +
+                                                                y][self.x_pos + x]
+                    if adj_cell.hidden:
+                        return False
+        
+        return True
+
+
 class Grid():
 
-    def __init__(self, gameId: int) -> None:
-        self.gameId = gameId
-        self.overall_tiles = GAMES[self.gameId].settings["width"] * \
-            GAMES[self.gameId].settings["height"]
-        self.num_of_mines = int(GAMES[self.gameId].settings["width"] *
-                                GAMES[self.gameId].settings["height"] * GAMES[self.gameId].settings["mines"] / 100)
+    def __init__(self) -> None:
+        self.overall_tiles = GAME.settings["width"] * \
+            GAME.settings["height"]
+        self.num_of_mines = int(GAME.settings["width"] *
+                                GAME.settings["height"] * GAME.settings["mines"] / 100)
         self.contents_created: bool = False
-        self.contents: List[List[Cell]] = [[Cell(self.gameId) for _ in range(
-            GAMES[self.gameId].settings["width"])] for _ in range(GAMES[self.gameId].settings["height"])]
+        self.contents: List[List[Cell]] = [[Cell() for _ in range(
+            GAME.settings["width"])] for _ in range(GAME.settings["height"])]
 
     def createLayout(self, x: int = None, y: int = None) -> None:
         """Creates the grid layout for the game and stores it in self.contents
@@ -173,22 +179,22 @@ class Grid():
 
         for i in range(self.overall_tiles):
             if i < self.num_of_mines:
-                arr[i] = Cell(self.gameId, -1)
+                arr[i] = Cell(-1)
             else:
-                arr[i] = Cell(self.gameId)
+                arr[i] = Cell()
 
         np.random.shuffle(arr)
         self.contents: List[List[Cell]] = list(
-            arr.reshape(GAMES[self.gameId].settings["height"], GAMES[self.gameId].settings["width"]))
+            arr.reshape(GAME.settings["height"], GAME.settings["width"]))
 
-        if GAMES[self.gameId].settings["easy_start"]:
+        if GAME.settings["easy_start"]:
             for b in range(-1, 2):
                 for a in range(-1, 2):
                     if self.contents[y + b][x + a].content != 0:
                         self.contents[y + b][x + a].content = 0
 
-        for y in range(GAMES[self.gameId].settings["height"]):
-            for x in range(GAMES[self.gameId].settings["width"]):
+        for y in range(GAME.settings["height"]):
+            for x in range(GAME.settings["width"]):
                 self.contents[y][x].x_pos = x
                 self.contents[y][x].y_pos = y
                 self.contents[y][x].content = self.checkAdjMines(x, y)
@@ -210,7 +216,7 @@ class Grid():
             # print("\nNew\n")
             for y in range(-1, 2):
                 for x in range(-1, 2):
-                    if 0 <= x_index + x < GAMES[self.gameId].settings["width"] and 0 <= y_index + y < GAMES[self.gameId].settings["height"]:
+                    if 0 <= x_index + x < GAME.settings["width"] and 0 <= y_index + y < GAME.settings["height"]:
                         if self.contents[y_index + y][x_index + x].content == -1:
                             # print("Mine at:", x_index + x, y_index + y)
                             num_of_adj_mines += 1
@@ -222,54 +228,29 @@ class Grid():
 
 class Game():
 
-    def __init__(self, id: int) -> None:
-        self.gameId = id
+    def __init__(self) -> None:
+        self.runing = True
+        self.playing = True
+        self.flagging = False
         self.settings = {
             "easy_start": True,
             "width": 20,
             "height": 25,
             "mines": 25,  # Percentage
-            "open_for_clients": False,
+            "open_for_clients": True,
             # Size of one cell (WARNING: make sure to change the images dimension as well)
             "cell_size": 32,
             "top_border_size": 100,  # Size of top border
             "lrb_border_size": 16,  # Left, Right, Bottom border
         }
-        self.settings_button = Cell(self.gameId, 14, False)
-        self.restart_button = Cell(self.gameId, 10, False)
+        self.settings_button = Cell(14, False)
+        self.restart_button = Cell(10, False)
         self.settings_pos = (self.settings["width"] - 0.75, -2.75)
         if self.settings["width"] % 2 == 0:
             self.restart_pos = (self.settings["width"] / 2 - 0.5, -2)
         else:
             self.restart_pos = self.settings["width"] / 2
 
-    def listener(self):
-
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}")
-                while self.playing:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    data = str(data, "ascii").split(", ")
-                    print(data)
-                    try:
-                        if data[2] == "hide":
-                            self.grid.contents[int(data[0])][int(
-                                data[1])].hidden = True
-                        elif data[2] == "reveal":
-                            self.grid.contents[int(data[0])][int(
-                                data[1])].revealCell()
-                        elif data[2] == "flag":
-                            self.flag(int(data[0]), int(data[1]))
-                        conn.sendall(b"Success!")
-                    except Exception as e:
-                        print(e)
-                        conn.sendall(b"Fail!")
 
     def highlightCell(self, event: pygame.event.Event):
         x = (event.pos[0] - self.settings["lrb_border_size"]) / \
@@ -281,6 +262,7 @@ class Game():
         elif self.restart_button.content != 10:
             self.restart_button.content = 10
 
+
     def findAffectedCell(self, event: pygame.event.Event = None):
         """Reacts to player left click
 
@@ -291,21 +273,86 @@ class Game():
             self.settings["cell_size"]
         y = (event.pos[1] - self.settings["top_border_size"]) / \
             self.settings["cell_size"]
-        if self.settings["top_border_size"] < event.pos[1] < self.disp_height - self.settings["lrb_border_size"] and self.settings["lrb_border_size"] < event.pos[0] < self.disp_width - self.settings["lrb_border_size"]:
-            x = floor(x)
-            y = floor(y)
-            actions = {1: self.reveal,
-                       3: self.flag,
-                       }
-            actions[event.button](x, y)
-        elif round(x) == round(self.restart_pos[0]) and floor(y) == self.restart_pos[1]:
+        if self.playing:
+            if self.settings["top_border_size"] < event.pos[1] < self.disp_height - self.settings["lrb_border_size"] and self.settings["lrb_border_size"] < event.pos[0] < self.disp_width - self.settings["lrb_border_size"]:
+                x = floor(x)
+                y = floor(y)
+                actions = {1: self.reveal,
+                        3: self.flag,
+                        }
+                actions[event.button](x, y)
+
+        if round(x) == round(self.restart_pos[0]) and floor(y) == self.restart_pos[1]:
             self.restart()
 
+    def gameLoop(self):
 
+        while self.runing:
+
+            GAME.gameDisplay.fill(BG_COLOR)
+            for y in range(GAME.settings["height"]):
+                for x in range(GAME.settings["width"]):
+                    self.grid.contents[y][x].drawCell(x, y)
+
+            self.settings_button.drawCell(
+                self.settings_pos[0], self.settings_pos[1])
+            self.restart_button.drawCell(
+                self.restart_pos[0], self.restart_pos[1])
+
+            pygame.display.update()
+            for event in pygame.event.get():
+                try:
+                    Game.event_types[event.type](self, event)
+                except Exception as e:
+                    pass
+
+        pygame.quit()
+
+
+    def client(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(b'game')
+            print(str(s.recv(2048), "ascii"))
+            while self.runing:
+                try:
+                    data = str(s.recv(2048), "ascii")
+                    data = data.split()
+                    if data[0] == "init":
+                        s.sendall(b'connected ' + bytes(data[1], 'ascii'))
+                        # s.sendall(b'connected to client' + bytes(data[1], 'ascii'))
+                    print(data)
+                    if not data:
+                        break
+                except Exception as e:
+                    print(e)
+
+
+    def play(self):
+        
+        self.disp_width = self.settings["cell_size"] * self.settings["width"] + \
+            self.settings["lrb_border_size"] * 2  # Display width
+        self.disp_height = self.settings["cell_size"] * self.settings["height"] + \
+            self.settings["lrb_border_size"] + \
+            self.settings["top_border_size"]  # Display height
+
+        self.gameDisplay = pygame.display.set_mode(
+            (self.disp_width, self.disp_height))  # Create display
+        pygame.display.set_caption("Minesweeper")
+        
+        self.grid = Grid()
+        self.drawThread = threading.Thread(target=self.gameLoop)
+        self.clientThread = threading.Thread(target=self.client)
+        self.drawThread.start()
+        self.clientThread.start()
+        if self.settings["open_for_clients"]:
+            pass
+        # TODO
     def reveal(self, x: int, y: int):
         if not self.grid.contents_created:
             self.grid.createLayout(x, y)
         self.grid.contents[y][x].revealCell()
+
 
     def flag(self, x: int, y: int):
         """Toggles the flagging option.
@@ -318,58 +365,19 @@ class Game():
         else:
             self.grid.contents[y][x].flagged = True
 
+
     def restart(self):
-        self.grid = Grid(self.gameId)
+        self.grid = Grid()
+        self.playing = True
+
 
     def exit(self, event: pygame.event.Event):
         """Exits the game
         """
-        self.playing = False
+        self.runing = False
 
-    def play(self):
-        self.drawThread = threading.Thread(target=self.gameLoop)
-        self.drawThread.start()
-        if self.settings["open_for_clients"]:
-            self.listenThread = threading.Thread(target=self.listener)
-            self.listenThread.start()
 
-    def gameLoop(self):
 
-        self.disp_width = self.settings["cell_size"] * self.settings["width"] + \
-            self.settings["lrb_border_size"] * 2  # Display width
-        self.disp_height = self.settings["cell_size"] * self.settings["height"] + \
-            self.settings["lrb_border_size"] + \
-            self.settings["top_border_size"]  # Display height
-
-        self.gameDisplay = pygame.display.set_mode(
-            (self.disp_width, self.disp_height))  # Create display
-        pygame.display.set_caption("Minesweeper")
-
-        self.playing = True
-        self.flagging = False
-        self.grid = Grid(self.gameId)
-
-        while self.playing:
-
-            GAMES[self.gameId].gameDisplay.fill(BG_COLOR)
-            for y in range(GAMES[self.gameId].settings["height"]):
-                for x in range(GAMES[self.gameId].settings["width"]):
-                    self.grid.contents[y][x].drawCell(x, y)
-
-            self.settings_button.drawCell(
-                self.settings_pos[0], self.settings_pos[1])
-            self.restart_button.drawCell(
-                self.restart_pos[0], self.restart_pos[1])
-
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                try:
-                    Game.event_types[event.type](self, event)
-                except Exception as e:
-                    e
-
-        pygame.quit()
 
     event_types = {
         pygame.MOUSEBUTTONDOWN: findAffectedCell,
@@ -377,7 +385,5 @@ class Game():
         pygame.QUIT: exit
     }
 
-
-GAMES: List[Game] = []
-GAMES.append(Game(0))
-GAMES[0].play()
+GAME = Game()
+GAME.play()
