e519b383c11e2845db3297157e1dcb7a982dd8df
liad <liad101112@gmail.com>
2022-12-26 03:07:03 +0200

    minor changes

--- bot/server.py
+++ bot/server.py
@@ -8,34 +8,55 @@ HOST = '10.100.102.24'
 PORT = 65432
 RECVR: Client = None
 
-ServerSideSocket = socket.socket()
-try:
-    ServerSideSocket.bind((HOST, PORT))
-except socket.error as e:
-    print(str(e))
-print('Socket is listening..')
-ServerSideSocket.listen(5)
+class Server():
+
+    def __init__(self) -> None:
+        self.server_socket = socket.socket()
+        self.threads = 0
+        try:
+            self.server_socket.bind((HOST, PORT))
+        except socket.error as e:
+            print(str(e))
+        print('Socket is listening..')
+        self.server_socket.listen(5)
 
 
-def inputListener():
+    def inputListener(self):
 
-    global RECVR, ServerSideSocket
+        global RECVR
 
-    comm = Communicator()
-    while True:
-        inp = input().split(maxsplit=1)
-        if inp[0] != "quit":
-            if inp[0] == "client":
-                RECVR = CLIENTS[int(inp[1])]
-            
-            if RECVR is not None:
-                comm.pipe(RECVR, inp[0] + " " + inp[1])
+        comm = Communicator()
+        while True:
+            inp = input().split(maxsplit=1)
+            if inp[0] != "quit":
+                if inp[0] == "client":
+                    RECVR = CLIENTS[int(inp[1])]
+                
+                if RECVR is not None:
+                    comm.pipe(RECVR, inp[0] + " " + inp[1])
+                else:
+                    print("Can't execute command as no client was chosen")
             else:
-                print("Can't execute command as no client was chosen")
-        else:
-            ServerSideSocket.close()
-            break
+                self.server_socket.close()
+                break
 
+    def run(self):
+
+        threading.Thread(target=self.inputListener).start()
+        with self.server_socket:
+            while True:
+                clientconn, address = self.server_socket.accept()
+                print('Connected to: ' + address[0] + ':' + str(address[1]))
+                client = Client(self.threads, str(
+                    clientconn.recv(2048), 'ascii'), clientconn)
+                client.socket.sendall(bytes('Server recognizes you as %s %s' % (client.type, client.id), 'ascii'))
+                if self.threads == 0:
+                    RECVR = client
+                threading.Thread(target=CLIENT_TYPES[client.type], args=(client,)).start()
+                print("%s is connected & handled by thread %s" %
+                    (client.type, client.id))
+                CLIENTS.append(client)
+                self.threads += 1
 
 def mineSweeper(client: Client):
 
@@ -60,21 +81,5 @@ CLIENT_TYPES: Dict[str, callable] = {"minesweeper": mineSweeper,
                                      "snake": snake
                                      }
 
-NUM_OF_THREADS = 0
-threading.Thread(target=inputListener).start()
-with ServerSideSocket:
-    while True:
-        clientconn, address = ServerSideSocket.accept()
-        print('Connected to: ' + address[0] + ':' + str(address[1]))
-        client = Client(NUM_OF_THREADS, str(
-            clientconn.recv(2048), 'ascii'), clientconn)
-        client.socket.sendall(bytes('Server recognizes you as %s %s' % (client.type, client.id), 'ascii'))
-        if NUM_OF_THREADS == 0:
-            RECVR = client
-        threading.Thread(target=CLIENT_TYPES[client.type], args=(client,)).start()
-        print("%s is connected & handled by thread %s" %
-              (client.type, client.id))
-        CLIENTS.append(client)
-        NUM_OF_THREADS += 1
 
 
